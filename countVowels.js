6adfbf004b50362c19ecbf58d905a4c408edc978
JB1 <28571886+jayone11@users.noreply.github.com>
2023-01-17 13:24:58 -0800

    adding comments and the initial skeleton of the script

--- countVowels.js
+++ countVowels.js
@@ -3,3 +3,12 @@ let input = document.querySelector("#input");
 let submit = document.querySelector("#submit");
 let answer = document.querySelector("#submit");
 
+// Create an event listener for the submit button
+submit.addEventListener("click", function(){
+
+});
+
+// Create a function that takes in a string, counts the vowels, and displays the answer
+function countVowels(str){
+
+};
