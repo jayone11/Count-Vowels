f5f1919f4509ddd74eac48caf04f2eb92e5d29b4
JB1 <28571886+jayone11@users.noreply.github.com>
2023-01-17 13:18:56 -0800

    defined the input, submit elements, and p tag for the answer

--- countVowels.js
+++ countVowels.js
@@ -1,1 +1,4 @@
 // Define the input and submit elements, and the p tag where the answer will display
+let input = document.querySelector("#input");
+let sumbmit = document.querySelector("#submit");
+let answer = document.querySelector("#submit");
